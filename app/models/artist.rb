c58c7f1133b0eaf30a713ca494a42e2e6f462d79
mayalexandera <maya.blevins@gmail.com>
2020-06-08 09:44:05 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,42 @@
 class Artist
+  @@all = []
 
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map { |painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map { |gallery| gallery.city}
+  end
+
+  def self.total_experience
+    Artist.all.inject(0){ |sum, e| sum + e.years_experience}
+  end
+
+  def self.most_prolific
+    Artist.all.max_by { |artist| artist.paintings.length / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,24 @@
 require_relative '../config/environment.rb'
 
+artist1 = Artist.new("george bush", 3)
+artist2 = Artist.new("tom", 1)
+artist3 = Artist.new("beth", 2)
+artist4 = Artist.new("davinci", 30)
+artist5 = Artist.new("apple paltrow", 4)
+artist6 = Artist.new("elon musk", 10)
 
+gallery1 = Gallery.new("MCAD", "Minneapolis")
+gallery2 = Gallery.new("SAIC", "Chicago")
+gallery3 = Gallery.new("Metropolitan", "NYC")
+gallery4 = Gallery.new("MAC","Milwaukee")
 
+painting1 = Painting.new("untitled", 3000, artist1, gallery1)
+painting2 = Painting.new("feet", 100, artist2, gallery2)
+painting3 = Painting.new("apples and bananas", 250000, artist3, gallery3)
+painting4 = Painting.new("sleepy", 250, artist4, gallery4)
+painting5 = Painting.new("is this the end?", 14, artist5, gallery1)
+painting6 = Painting.new("060220", 100, artist6, gallery2)
+painting7 = Painting.new("Impossible", 1500, artist1, gallery2)
+painting7 = Painting.new("Meatheads", 660, artist4, gallery4)
 
-binding.pry
-
-puts "Bob Ross rules."
+Pry.start
